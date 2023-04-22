9bd48ac18b7feec28fdeb8b4362f907e7cf73aec
changon <tyu4778@gmail.com>
2023-04-22 23:40:44 +0900

    Update: React and TypeScript

--- Typescript/TypeScript Master with Webpack and React/17.shopping-app/src/App.tsx
+++ Typescript/TypeScript Master with Webpack and React/17.shopping-app/src/App.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function func<T>(x: T): T {
+  return x;
+}
+
+const arrowFunc = <T,>(x: T): T => {
+  return x;
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO WORLD!</h1>
     </div>
   );
 }
