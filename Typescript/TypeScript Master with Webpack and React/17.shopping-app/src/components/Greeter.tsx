60f35c4ca0fd3addb6886afd6f1ef255a168cdd5
changon <tyu4778@gmail.com>
2023-04-28 23:59:21 +0900

    Feat: React.FC 사용법

--- Typescript/TypeScript Master with Webpack and React/17.shopping-app/src/components/Greeter.tsx
+++ Typescript/TypeScript Master with Webpack and React/17.shopping-app/src/components/Greeter.tsx
@@ -4,4 +4,9 @@ const Greeter = (): JSX.Element => {
     return <h1>Hello!</h1>;
 }
 
+// 기존에 사용하던 React.FC : 현재는 조금 오래되었고 권장하지 않는다.
+// const Greeter: React.FC = () => {
+//     return <h1>Hello!~~~~~~</h1>;
+// }
+
 export default Greeter;
