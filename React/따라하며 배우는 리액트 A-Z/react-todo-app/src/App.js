aadc16dbbbd2402d42af1836bb57adaef66c6208
changon <tyu4778@gmail.com>
2023-05-08 23:03:02 +0900

    Feat: 할 일 목록 앱 UI 작성

--- React/따라하며 배우는 리액트 A-Z/react-todo-app/src/App.js
+++ React/따라하며 배우는 리액트 A-Z/react-todo-app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  // 변수로 css 설정
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  }
+
+  // 함수로 css 설정
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  // 할 일 목록 데이터
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    }
+  ]
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+          {/* */}
+          {this.todoData.map((data) => (
+              <div style={this.getStyle()} key={data.id}>
+                <input type="checkbox" defaultChecked={false} />
+                  {data.title}
+                <button style={this.btnStyle}>x</button>
+              </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
