212d025cf8f217f12546bb5f9eae417f7bf4ce08
changon <tyu4778@gmail.com>
2023-05-09 22:34:06 +0900

    Feat: TO-DO List 삭제기능 추가

--- React/따라하며 배우는 리액트 A-Z/react-todo-app/src/App.js
+++ React/따라하며 배우는 리액트 A-Z/react-todo-app/src/App.js
@@ -23,18 +23,27 @@ export default class App extends Component {
   }
 
   // 할 일 목록 데이터
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    }
-  ]
+  state = {
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false
+      }
+    ],
+    value: "",
+  }
+
+  handleClick = (id) => {
+    // 클릭한 목록 지우기
+    let newTodoData = this.state.todoData.filter(data => data.id !== id);
+    this.setState({ todoData: newTodoData });
+  }
 
   render() {
     return (
@@ -43,12 +52,11 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {/* */}
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
               <div style={this.getStyle()} key={data.id}>
                 <input type="checkbox" defaultChecked={false} />
-                  {data.title}
-                <button style={this.btnStyle}>x</button>
+                  {data.title}                {/* 클릭 이벤트 발생시 함수 호출 */}
+                <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
               </div>
           ))}
         </div>
