ee2e4bc9b6a1248a9bcb3f9018795029bf25a0da
changon <tyu4778@gmail.com>
2024-01-24 23:40:26 +0900

    ✨ feat(search): 검색 페이지 구현
    -  useLocation을 이용한 검색 페이지 구현

--- React/react-a-to-z-tutorial/react-netflix-clone/src/components/Nav.js
+++ React/react-a-to-z-tutorial/react-netflix-clone/src/components/Nav.js
@@ -1,40 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Nav.css"
+import {useNavigate} from "react-router-dom";
 
 export default function Nav() {
-  const [show, setShow] = useState(false);
+	const [show, setShow] = useState(false);
+	const [searchValue, setSearchValue] = useState(""); // 검색 내용
+	const navigate = useNavigate(); // 페이지 이동시 사용
 
-  useEffect(() => {
-    // addEventListener
-    window.addEventListener("scroll", () => {
-      if(window.scrollY > 50) {
-        setShow(true);
-      } else {
-        setShow(false);
-      }
-    });
-  
-    return () => {
-      // removeEventListener
-      window.removeEventListener("scroll", () => {});
-    }
-  }, []);
-  
+	useEffect(() => {
+		// addEventListener
+		window.addEventListener("scroll", () => {
+			if (window.scrollY > 50) {
+				setShow(true);
+			} else {
+				setShow(false);
+			}
+		});
 
-  return (
-    // show가 true이면 class에 nav__black 추가
-    <nav className={`nav ${show && "nav__black"}`}>
-     <img
-      alt="Netflix logo"
-      src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
-      className="nav__logo"
-      onClick={() => window.location.reload()}
-    />
-    <img
-      alt="User logged"
-      src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
-      className="nav__avatar"
-    />
-    </nav>
-  )
+		return () => {
+			// removeEventListener
+			window.removeEventListener("scroll", () => {
+			});
+		}
+	}, []);
+
+	/**
+	 * 검색어 변경시 작동 함수
+	 * @param e
+	 */
+	const handleChange = (e) => {
+		setSearchValue(e.target.value); // 검색어 내용 변경
+		navigate(`search?q=${e.target.value}`); //
+	}
+
+	return (
+		// show가 true이면 class에 nav__black 추가
+		<nav className={`nav ${show && "nav__black"}`}>
+			<img
+				alt="Netflix logo"
+				src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg'
+				className="nav__logo"
+				onClick={() => window.location.reload()}
+			/>
+			<input value={searchValue} onChange={handleChange} className="nav__input" placeholder="영화를 검색해주세요."/>
+			<img
+				alt="User logged"
+				src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+				className="nav__avatar"
+			/>
+		</nav>
+	)
 }
--- React/react-a-to-z-tutorial/react-netflix-clone/src/pages/SearchPage/index.js
+++ React/react-a-to-z-tutorial/react-netflix-clone/src/pages/SearchPage/index.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from "react-router-dom";
+import axios from "../../api/axios";
 
 function SearchPage() {
-  return (
-    <div>SearchPage</div>
-  )
+	const [searchResults, setSearchResults] = useState([]);
+
+	console.log('useLocation()', useLocation());
+	const useQuery = () => {
+		return new URLSearchParams(useLocation().search);
+	}
+
+	let query = useQuery();
+	const searchTerm = query.get("q");
+	console.log(searchTerm);
+
+	useEffect(() => {
+		if(searchTerm) {
+			fetchSearchMovie(searchTerm);
+		}
+	}, [searchTerm]);
+
+	/**
+	 * 영화검색
+	 * @param searchTerm : 영화검색어
+	 * @returns {Promise<void>}
+	 */
+	const fetchSearchMovie = async (searchTerm) => {
+		try {
+			const request = await axios.get(
+				`/search/multi?include_adult=false&query=${searchTerm}` // 성인 영화 제외
+			)
+			console.log(request);
+			setSearchResults(request.data.results);
+		} catch (e) {
+			console.log("error", e);
+		}
+	}
+
+	return (
+		<div></div>
+	)
 }
 
 export default SearchPage;
