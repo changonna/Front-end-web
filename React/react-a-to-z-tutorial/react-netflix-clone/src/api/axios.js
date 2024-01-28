4313a35e9cdbabc746687bc0463c0ffbbbf8bef1
changon <tyu4778@gmail.com>
2024-01-28 23:21:59 +0900

    ✨ feat: .env 생성하여 api_key 저장

--- React/react-a-to-z-tutorial/react-netflix-clone/src/api/axios.js
+++ React/react-a-to-z-tutorial/react-netflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "537124f0c7cb9457158a71e716d2af4e",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   }
 });
