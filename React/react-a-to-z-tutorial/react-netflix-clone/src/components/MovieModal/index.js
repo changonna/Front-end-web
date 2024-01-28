7d0fe1b640936d7b1bdde3890bd64a7b209364c8
changon <tyu4778@gmail.com>
2024-01-28 22:33:37 +0900

    ✨ feat: Modal창 외부 클릭시 닫기 기능 추가
    - Modal 창 외부 클릭시 모달 닫게 만드는 Custom Hooks 생성

--- React/react-a-to-z-tutorial/react-netflix-clone/src/components/MovieModal/index.js
+++ React/react-a-to-z-tutorial/react-netflix-clone/src/components/MovieModal/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 import "./MovieModal.css";
+import React, { useRef } from 'react';
 
 function MovieModal({
   backdrop_path,
@@ -11,10 +12,13 @@ function MovieModal({
   vote_average,
   setModalOpen
 }) {
+  const ref = useRef();
+  useOnClickOutside(ref, () => {setModalOpen(false)});
+
   return (
     <div className='presentation'>
       <div className='wrapper-modal'>
-        <div className='modal'>
+        <div className='modal' ref={ref}>
           {/** x클릭시 modalOpen false로 변경하여 끄기 **/}
           <span onClick={() => setModalOpen(false)} className='modal-close'>
             X
