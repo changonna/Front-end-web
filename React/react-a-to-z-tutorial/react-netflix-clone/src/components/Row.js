80f6d1a8452806dbdcdbda9855ec6f315ee5e98b
changon <tyu4778@gmail.com>
2024-01-28 22:59:20 +0900

    ✨ feat: 영화 목록 터치 슬라이드 구현
    - swiper 모듈을 이용한 터치 슬라이드 구현

--- React/react-a-to-z-tutorial/react-netflix-clone/src/components/Row.js
+++ React/react-a-to-z-tutorial/react-netflix-clone/src/components/Row.js
@@ -3,6 +3,17 @@ import axios from "../api/axios";
 import "./Row.css";
 import MovieModal from './MovieModal';
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function Row({ title, isLargeRow, id, fetchUrl }) {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -28,33 +39,46 @@ export default function Row({ title, isLargeRow, id, fetchUrl }) {
     <section className='row'>
       {/** TITLE */}
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span className='arrow'
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}>{"<"}</span>
-        </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         <div id={id} className='row__posters'>
           {/**SEVERAL ROW__POSTER */}
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              alt={movie.name}
-              onClick={() => handleClick(movie)} // 클릭시 이벤트 함수 실행
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                alt={movie.name}
+                onClick={() => handleClick(movie)} // 클릭시 이벤트 함수 실행
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
-          <span className='arrow'
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}>{">"}</span>
-        </div>
-      </div>
-
+      </Swiper>
       {
         // modalOpen이 true일때, MovieModal을 보여준다.
         modalOpen && ( // props로 movie정보와, setModalOpen을 넘겨준다.
